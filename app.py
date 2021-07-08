b083262eb935d7793bd8ee4837734adbee8fe77b
Elvira <e.kulikova@norsi-trans.ru>
2021-07-08 13:58:30 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+posts = {
+    0: {
+        'title': 'Hello, world',
+        'content': 'This is my first blog post!'
+    }
+}
+
+
+@app.route('/')
+def home():
+    return 'Hello, world!'
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = posts.get(post_id)
+    #return f"Post {post['title']}, content: {post['content']}"
+    return render_template('post.html', post=post)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
