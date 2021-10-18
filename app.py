a2a5f10ec6fa17afb33c964734ad6646af6d93b2
evulet1 <elvirochkata@gmail.com>
2021-10-18 16:27:32 +0300

    form post

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ def form():
     return render_template('create.jinja2')
 
 
-@app.route('/post/create')
+@app.route('/post/create', methods=['POST'])
 def create():
-    title = request.args.get('title')
-    content = request.args.get('content')
+    title = request.form.get('title')
+    content = request.form.get('content')
     post_id = len(posts)
     posts[post_id] = {'id': post_id, 'title': title, 'content': content}
 
