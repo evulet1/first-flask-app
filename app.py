6ec193f99095cce3208da40833a37fe0ad41a565
evulet1 <elvirochkata@gmail.com>
2021-10-18 16:31:24 +0300

    two methods in one function

--- app.py
+++ app.py
@@ -24,19 +24,16 @@ def post(post_id):
     return render_template('post.html', post=post)
 
 
-@app.route('/post/form')
-def form():
-    return render_template('create.jinja2')
-
-
-@app.route('/post/create', methods=['POST'])
+@app.route('/post/create', methods=['GET', 'POST'])
 def create():
-    title = request.form.get('title')
-    content = request.form.get('content')
-    post_id = len(posts)
-    posts[post_id] = {'id': post_id, 'title': title, 'content': content}
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        post_id = len(posts)
+        posts[post_id] = {'id': post_id, 'title': title, 'content': content}
 
-    return redirect(url_for('post', post_id=post_id))
+        return redirect(url_for('post', post_id=post_id))
+    return render_template('create.jinja2')
 
 
 if __name__ == '__main__':
